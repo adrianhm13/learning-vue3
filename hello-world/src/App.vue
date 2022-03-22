116a39ac493ccf5841b822128768e4d47a7b8e9c
Adrian Hervas <adrian.hervasmartinez@gmail.com>
2022-03-22 12:00:42 +0100

    Binding text

---
+++ hello-world/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <p>{{ greet }} {{ name }}</p>
+    <div v-text="channel"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      greet: "Hello",
+      name: "Adrian",
+      channel: "AdrianProgrammer",
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
