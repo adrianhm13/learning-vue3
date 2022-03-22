1cad3346b6cebb58feeb448016e54316bad6737e
Adrian Hervas <adrian.hervasmartinez@gmail.com>
2022-03-22 12:10:33 +0100

    Binding Atributtes

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -2,6 +2,8 @@
   <div>
     <p>{{ greet }} {{ name }}</p>
     <div v-html="channel"></div>
+    <h2 v-bind:id="headingId">Heading</h2>
+    <button v-bind:disabled="isDisabled">Bind</button>
   </div>
 </template>
 
@@ -13,6 +15,8 @@ export default {
       greet: "Hello",
       name: "Adrian",
       channel: "<b>Adrian Developer</b>",
+      headingId: "heading",
+      isDisabled: false,
     };
   },
 };
