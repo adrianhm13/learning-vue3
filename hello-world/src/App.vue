1b6e753c50e4895d25c61a1dfb6fc492a9f7fa7f
Adrian Hervas <adrian.hervasmartinez@gmail.com>
2022-03-22 17:04:28 +0100

    Conditional Rendering, v-show, v-if

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -5,10 +5,14 @@
     <h2 v-else-if="num > 0">The number is positive</h2>
     <h2 v-else>Not a number</h2>
     <template v-if="display">
-    <h2>Adrian</h2>
-    <h2>Hervas</h2>
-    <h2>Vue</h2>
+      <h2>Adrian</h2>
+      <h2>Hervas</h2>
+      <h2>Vue</h2>
     </template>
+    <!-- Uses display to show/hide element -->
+    <h2 v-show="showElement">Using v-show</h2>
+    <!-- Mount / Unmount Element -->
+    <h2 v-if="showElement">Using v-if</h2>
 
   </div>
 </template>
@@ -20,6 +24,7 @@ export default {
     return {
       num: 5,
       display: true,
+      showElement: false,
     };
   },
 };
