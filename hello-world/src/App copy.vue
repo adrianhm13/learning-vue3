108a31aea1c839955647e12280fce43158936eeb
Adrian Hervas <adrian.hervasmartinez@gmail.com>
2022-03-22 15:59:03 +0100

    Conditional Rendering

---
+++ hello-world/src/App copy.vue
@@ -0,0 +1,86 @@
+<template>
+  <div>
+    <p>{{ greet }} {{ name }}</p>
+    <div v-html="channel"></div>
+    <h2 :id="headingId">Heading</h2>
+    <button :disabled="isDisabled">Bind</button>
+    <h2 class="underline">Underlined text</h2>
+    <h2 :class="status">Status</h2>
+    <h2 :class="isPromoted && 'promoted'">Promoted Movie</h2>
+    <h2 :class="isSoldOut ? 'sold-out' : 'new'">Soldout? movie</h2>
+    <h2 :class="['new', 'promoted']">Newly Promoted Movie</h2>
+    <h2 :class="[isPromoted && 'promoted', isSoldOut ? 'sold-out' : 'new']">
+      Array conditional movie
+    </h2>
+    <h2
+      :class="{
+        promoted: isPromoted,
+        new: !isSoldOut,
+        'sold-out': isSoldOut,
+      }"
+    >
+      Object conditional movie
+    </h2>
+    <h2
+      :style="{
+        color: highlightColor,
+        fontSize: headerSize + 'px',
+      }"
+    >
+      Inline Style
+    </h2>
+    <h2 :style="headerStyleObject">Styled Object</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      greet: "Hello",
+      name: "Adrian",
+      channel: "<b>Adrian Developer</b>",
+      headingId: "heading",
+      isDisabled: false,
+      status: "success",
+      isPromoted: true,
+      isSoldOut: true,
+      highlightColor: "orange",
+      headerSize: 50,
+      headerStyleObject: {
+        color: "orange",
+        fontSize: "50px",
+        padding: "20px",
+      },
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.underline {
+  text-decoration: underline;
+}
+
+.promoted {
+  font-style: italic;
+}
+
+.new {
+  color: olivedrab;
+}
+
+.sold-out {
+  color: red;
+}
+</style>
--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,37 +1,15 @@
 <template>
   <div>
-    <p>{{ greet }} {{ name }}</p>
-    <div v-html="channel"></div>
-    <h2 v-bind:id="headingId">Heading</h2>
-    <button v-bind:disabled="isDisabled">Bind</button>
-    <h2 class="underline">Underlined text</h2>
-    <h2 v-bind:class="status">Status</h2>
-    <h2 v-bind:class="isPromoted && 'promoted'">Promoted Movie</h2>
-    <h2 v-bind:class="isSoldOut ? 'sold-out' : 'new'">Soldout? movie</h2>
-    <h2 v-bind:class="['new', 'promoted']">Newly Promoted Movie</h2>
-    <h2
-      v-bind:class="[isPromoted && 'promoted', isSoldOut ? 'sold-out' : 'new']"
-    >
-      Array conditional movie
-    </h2>
-    <h2
-      v-bind:class="{
-        promoted: isPromoted,
-        new: !isSoldOut,
-        'sold-out': isSoldOut,
-      }"
-    >
-      Object conditional movie
-    </h2>
-    <h2
-      v-bind:style="{
-        color: highlightColor,
-        fontSize: headerSize + 'px',
-      }"
-    >
-      Inline Style
-    </h2>
-    <h2 v-bind:style="headerStyleObject">Styled Object</h2>
+    <h2 v-if="num === 0">The number is zero</h2>
+    <h2 v-else-if="num < 0">The number is negative</h2>
+    <h2 v-else-if="num > 0">The number is positive</h2>
+    <h2 v-else>Not a number</h2>
+    <template v-if="display">
+    <h2>Adrian</h2>
+    <h2>Hervas</h2>
+    <h2>Vue</h2>
+    </template>
+
   </div>
 </template>
 
@@ -40,21 +18,8 @@ export default {
   name: "App",
   data() {
     return {
-      greet: "Hello",
-      name: "Adrian",
-      channel: "<b>Adrian Developer</b>",
-      headingId: "heading",
-      isDisabled: false,
-      status: "success",
-      isPromoted: true,
-      isSoldOut: true,
-      highlightColor: "orange",
-      headerSize: 50,
-      headerStyleObject: {
-        color: "orange",
-        fontSize: "50px",
-        padding: "20px",
-      },
+      num: 5,
+      display: true,
     };
   },
 };
@@ -68,21 +33,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-
-.underline {
-  text-decoration: underline;
-}
-
-.promoted {
-  font-style: italic;
-}
-
-.new {
-  color: olivedrab;
-}
-
-.sold-out {
-  color: red;
 }
 </style>
