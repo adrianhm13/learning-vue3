2aae0ea8cc37d08d16d9de8b0184449ed3896b33
Adrian Hervas <adrian.hervasmartinez@gmail.com>
2022-03-24 11:23:17 +0100

    Added a new route

--- router-project/src/views/HomeView.vue
+++ router-project/src/views/HomeView.vue
@@ -1,18 +1,32 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut impedit dolorum
+    accusantium nisi doloribus rem obcaecati. Odit quia necessitatibus dicta
+    delectus alias. Optio quia tempore hic earum consequatur quas expedita enim
+    veritatis, nam deleniti nemo pariatur, ratione rem commodi, alias provident
+    porro. Ipsum iste repudiandae laudantium unde voluptatibus. Illo enim unde
+    tempora aliquid cumque architecto perferendis fugit blanditiis hic, in
+    laboriosam odio iure corrupti fuga nobis dolorum sunt ea vitae deleniti esse
+    eum quo voluptates corporis iusto. Animi placeat nostrum consequatur nisi
+    soluta? Aspernatur excepturi incidunt ipsa ab at facilis deleniti odio, ea
+    cumque pariatur libero labore ullam ratione quaerat. Lorem ipsum dolor sit
+    amet consectetur adipisicing elit. Ut impedit dolorum accusantium nisi
+    doloribus rem obcaecati. Odit quia necessitatibus dicta delectus alias.
+    Optio quia tempore hic earum consequatur quas expedita enim veritatis, nam
+    deleniti nemo pariatur, ratione rem commodi, alias provident porro. Ipsum
+    iste repudiandae laudantium unde voluptatibus. Illo enim unde tempora
+    aliquid cumque architecto perferendis fugit blanditiis hic, in laboriosam
+    odio iure corrupti fuga nobis dolorum sunt ea vitae deleniti esse eum quo
+    voluptates corporis iusto. Animi placeat nostrum consequatur nisi soluta?
+    Aspernatur excepturi incidunt ipsa ab at facilis deleniti odio, ea cumque
+    pariatur libero labore ullam ratione quaerat.
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  components: {},
+};
 </script>
