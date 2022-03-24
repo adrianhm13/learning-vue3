b803baa3764d747ae07a297ef5f9f6495f47eaa9
Adrian Hervas <adrian.hervasmartinez@gmail.com>
2022-03-24 13:20:04 +0100

    Fetching process and conditional rendering

--- router-project/src/views/jobs/JobDetails.vue
+++ router-project/src/views/jobs/JobDetails.vue
@@ -1,14 +1,28 @@
 <template>
-  <div>
+  <div v-if="job">
     <h1>Jobs Detail Page</h1>
-    <p>The job id is {{ id }}</p>
+    <p>{{ job.title }}</p>
+    <p>{{ job.details }}</p>
+  </div>
+  <div v-else>
+    <p>Job it's being fetched</p>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['id'],
-
+  props: ["id"],
+  data() {
+    return {
+      job: null,
+    };
+  },
+  mounted() {
+    fetch(`http://localhost:3000/jobs/${this.id}`)
+      .then((res) => res.json())
+      .then((data) => (this.job = data))
+      .catch((error) => console.log(error.message));
+  },
   // data() {
   //   return {
   //     id: this.$route.params.id,
