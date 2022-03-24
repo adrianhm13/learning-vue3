60ea4fc463fa685be527acf8c7621b252eddd6f4
Adrian Hervas <adrian.hervasmartinez@gmail.com>
2022-03-24 08:28:56 +0100

    Demostration lifecycle hooks

--- reaction-timer/src/App.vue
+++ reaction-timer/src/App.vue
@@ -1,11 +1,27 @@
 <template>
-<h1>Reaction Timer</h1>
+  <h1>Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <BlockContainer v-if="isPlaying" :delay="delay"/>
 </template>
 
 <script>
+import BlockContainer from "./components/BlockContainer";
+
 export default {
   name: "App",
-  components: {},
+  components: { BlockContainer },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+    };
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+    },
+  },
 };
 </script>
 
